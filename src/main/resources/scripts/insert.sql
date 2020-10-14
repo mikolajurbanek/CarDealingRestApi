7a0b6e721c0808a32a39f40143e0e50e5949266c
Tomek K <tomek.krogulec@o2.pl>
2020-10-14 11:03:35 +0200

    added insert.sql

---
+++ src/main/resources/scripts/insert.sql
@@ -0,0 +1,2 @@
+insert into car_dealers(location, name, phonenumber) values('Warszawa', 'tomek', '123456789');
+insert into cars(automat_gear, brand, color, model, year, dealer_id) values(true, 'tesla', 'blue', 'model 3', 2020, 1);
